4dec0f90e63882ac7701ed79f07aad5de1479635
Kostas <kkarasavvas@gmail.com>
2023-09-07 15:02:23 +0300

    back to default 88 cols per line for fuff

--- neovim/custom/configs/null-ls.lua
+++ neovim/custom/configs/null-ls.lua
@@ -3,13 +3,15 @@ local null_ls = require('null-ls')
 
 local opts = {
   sources = {
-    null_ls.builtins.formatting.black.with({
-      extra_args = { "--line-length=120" },
-    }),
+    null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.mypy,
-    null_ls.builtins.diagnostics.ruff.with({
-      extra_args = { "--line-length=120" },
-    }),
+    null_ls.builtins.diagnostics.ruff,
+--    null_ls.builtins.formatting.black.with({
+--      extra_args = { "--line-length=120" },
+--    }),
+--    null_ls.builtins.diagnostics.ruff.with({
+--      extra_args = { "--line-length=120" },
+--    }),
   },
   on_attach = function (client, bufnr)
     if client.supports_method("textDocument/formatting") then
