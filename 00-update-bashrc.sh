ec2f0bc67d7bf6b1705cc66e6fc225b8b92e429c
Kostas <kkarasavvas@gmail.com>
2023-09-18 16:27:48 +0300

    added n shell commands for note taking

--- 00-update-bashrc.sh
+++ 00-update-bashrc.sh
@@ -1,15 +1,23 @@
 cat << 'EOF' >> ~/.bashrc
 
+# set note taking shortcuts
+n() { $EDITOR ~/MEGAsync/mine/.notes/"$*"; }
+nls() { ls -tr  ~/MEGAsync/mine/.notes/ | grep "$*"; }
+nr() { rm ~/MEGAsync/mine/.notes/"$*"; }
+ns() { grep -C 5 "$*" ~/MEGAsync/mine/.notes/*; }
+nv() { cat ~/MEGAsync/mine/.notes/"$*"; }
+
 # my aliases
 alias bc='bc -l'
 alias l='ls -ltr'
 alias bit='bitcoin-cli'
 alias gitcg="git log --all --decorate --oneline --graph"
-alias v='nvim.appimage'
+alias e='nvim.appimage'
+alias v='okular'
 
 # my init paths
 export PATH=$HOME/.local/bin:$PATH
 export PASSWORD_STORE_DIR=~/MEGAsync/mine/mine/.password-store/
-export EDITOR=v
+export EDITOR=nvim.appimage
 
 EOF
