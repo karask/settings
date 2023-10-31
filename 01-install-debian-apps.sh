253fb8a548db29e49d429faf2b3398d0dcfd7ca1
Kostas <kkarasavvas@gmail.com>
2023-10-31 16:46:22 +0200

    extra notes

--- 01-install-debian-apps.sh
+++ 01-install-debian-apps.sh
@@ -4,9 +4,12 @@ echo
 echo Install useful and required debian apps
 echo 
 
-sudo apt -y install wget curl git npm zim cmatrix firefox pass pdftk python3-venv 
+sudo apt -y install wget curl git npm zim cmatrix firefox pass pdftk python3-venv okular
 
 echo
-echo Note: python3-venv is required by mason (nvim) to install certain python plugins
+echo "Note: python3-venv is required by mason, nvim, to install certain python plugins"
+echo "Note: npm is also required by nvim"
+echo
+echo "Note: okular, from kde, is used for the generic viewer v alias."
 echo
 
