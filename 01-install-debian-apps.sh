f1e960c68fed7624fffc53a6ff4c71a6c5569d69
Kostas <kkarasavvas@gmail.com>
2024-02-13 20:02:10 +0200

    added python3-pip to installed packages

--- 01-install-debian-apps.sh
+++ 01-install-debian-apps.sh
@@ -4,7 +4,7 @@ echo
 echo Install useful and required debian apps
 echo 
 
-sudo apt -y install vim wget curl git npm zim htop cmatrix firefox pass pdftk python3-venv okular imagemagick
+sudo apt -y install vim wget curl git npm zim htop cmatrix firefox pass pdftk python3-venv python3-pip okular imagemagick
 
 echo
 echo "Note: python3-venv is required by mason, nvim, to install certain python plugins"
